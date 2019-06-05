d00cbf75c9ba1eee8f68c9ecbefa0c6ab301fdcc
Bora Guven <guvenbe@gmail.com>
2019-06-05 01:51:47 -0400

    calculte rate

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -1,13 +1,30 @@
 package guru.springframework;
 
+import java.util.HashMap;
+
 public class Bank {
 
+    private HashMap<Pair,Integer> rateMap = new HashMap<>();
 
     public Money reduce(Expression source, String toCurrency) {
 //       if (source instanceof Money) return  (Money) source;
 //        Sum sum =(Sum)source;
 //        return sum.reduce(toCurrency);
 
-        return source.reduce(toCurrency);
+        return source.reduce(this, toCurrency);
+    }
+
+    public int rate(String from, String to){
+        if (from.equals(to)){
+            return 1;
+        }
+
+       return rateMap.get(new Pair(from, to));
+
+       // return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from, to), rate);
     }
 }
