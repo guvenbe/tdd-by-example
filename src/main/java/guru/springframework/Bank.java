de8ad9acf50ab9d5e456ee8f2eaac85971c498da
Bora Guven <guvenbe@gmail.com>
2019-06-05 00:30:34 -0400

    added bank sum with reduce and expression

---
+++ src/main/java/guru/springframework/Bank.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+public class Bank {
+
+
+    public Money reduce(Expression source, String toCurrency) {
+//       if (source instanceof Money) return  (Money) source;
+//        Sum sum =(Sum)source;
+//        return sum.reduce(toCurrency);
+
+        return source.reduce(toCurrency);
+    }
+}
